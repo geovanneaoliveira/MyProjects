e17185ee62ec3c728ee5d8030f7a72cb4bd4233b
Geovanne <geovanne@vortexnet.com.br>
2023-06-05 15:44:04 -0300

    teste ottimizza

---
+++ Ottimiza/src/Main.java
@@ -0,0 +1,57 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+
+public class Main {
+    public static void main(String[] args) {
+        //Teste 1
+        System.out.println("2018 05 01");
+        //Teste 2
+        System.out.println(returnConcat(new String[]{"CARLOS","EDUARDO","DA","COSTA"}));
+        //Teste 3
+        System.out.println(returnGreatest(new int[]{334, 568, 123, 796, 256}));
+    }
+
+    //Questão 1
+    public static String returnFormattedDate(String dateString) {
+        DateTimeFormatterBuilder dateTimeFormatterBuilder = new DateTimeFormatterBuilder()
+                .append(DateTimeFormatter.ofPattern("[dd/MM/yyyy]" + "[dd-MM-yyyy]" + "[yyyy-MM-dd]" +"[dd/MM/yyyy]"
+                        +"[dd MM yyyy]"+"[MM/dd/yyyy]"+"[ddMMyyyy]"+"[yyyy MM dd]"));
+        DateTimeFormatter dateTimeFormatter = dateTimeFormatterBuilder.toFormatter();
+        LocalDate receivedDate = LocalDate.parse(dateString, dateTimeFormatter);
+        return receivedDate.format(DateTimeFormatter.ofPattern("yyyy MM"));
+    }
+
+    //Questão 2
+    public static String returnConcat(String[] strings){
+        if(strings.length!=0){
+            StringBuilder result = null;
+            for(int i = 0;i< strings.length;i++){
+                if (i==0){
+                    result = new StringBuilder(strings[0]);
+                } else {
+                    result.append(" ").append(strings[i]);
+                }
+            }
+            return result.toString();
+        } else {
+            return "Array vazio!";
+        }
+    }
+
+    //Questão 3
+    public static int returnGreatest (int[] ints){
+        if(ints.length!=0){
+            int greatest = -10000000;
+            for(int i = 0;i< ints.length;i++) {
+                if (ints[i] > greatest) {
+                    greatest = ints[i];
+                }
+            }
+            return greatest;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ Testes/testes.js
@@ -0,0 +1,33 @@
+
+    str = 'Happy birthday hennny!';
+    const originalStr = str.split(" ");
+    let wordsObj = [];
+    str = str.toLowerCase(); 
+    const strArray = str.split(" ");
+    for(let i = 0; i < strArray.length; i++){
+    var wObj = {};
+      for(let j = 0; j < strArray[i].length; j++){
+        var l = strArray[i].charAt(j);
+        wObj[l] = (isNaN(wObj[l]) ? 1 : wObj[l]+1);
+      }
+      wordsObj.push(wObj);
+    }
+    var num = 1;
+    var wordPos = -1;
+    for(let i = 0; i<wordsObj.length;i++){
+        let val = Object.values(wordsObj[i]);
+        var rep = Math.max(...val);
+        console.log(rep);
+        if(rep>num){
+            num = rep;
+            wordPos = i;
+        } 
+    }
+    if(wordPos === -1){
+        str = wordPos;
+    } else {
+        str = originalStr[wordPos];
+    }
+    console.log(str);
+    console.log(num);
+    console.log(wordPos);
